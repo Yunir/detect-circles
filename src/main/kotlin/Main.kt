b0a6a06af7a6a9ab7ec6ab5b9842c3de30fdb2ec
Yunir <yunir.me@gmail.com>
2020-03-23 21:07:24 +0300

    Implement reading the png file with pngj lib

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
+import ar.com.hjg.pngj.PngReader
+import java.io.File
 
 fun main() {
-    println("Hello!")
+    val pngr : PngReader = PngReader(File("src/main/resources/figures.png"))
+    println(pngr.toString())
 }
